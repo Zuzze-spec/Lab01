d8c377139b2ed8e1756f12ecbfc95a3fcaefb77a
Zuzze-spec <s268590@studenti.polito.it>
2026-02-24 14:24:50 +0100

    aggiunto il main

---
+++ main.py
@@ -0,0 +1,9 @@
+class Quiz:
+    def __init__(self, question, difficulty, correct_answer, wrong_answer1, wrong_answer2, wrong_answer3):
+        self.question = question
+        self.difficulty = difficulty
+        self.correct_answer = correct_answer
+        self.wrong_answer1 = wrong_answer1
+        self.wrong_answer2 = wrong_answer2
+        self.wrong_answer3 = wrong_answer3
+
